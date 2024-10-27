c278084c838a0862117ebaab5a6eb5b6004efb44
Chiper Andrei <138249161+chiperandrei42@users.noreply.github.com>
2024-10-27 11:49:20 +0200

    finished homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+let imageList = document.querySelector(".gallery");
+
+galleryItems.forEach(item => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("gallery__item");
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.href = item.original;
+
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.src = item.preview;
+    img.alt = item.description;
+
+    link.appendChild(img);
+    listItem.appendChild(link);
+    imageList.appendChild(listItem);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
